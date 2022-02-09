0c751d8ae730a8f46ae5f24acdcf2b7e582be63f
ryomaD1225 <ryoma20151225@gmail.com>
2022-02-10 03:03:44 +0900

    不要ファイル削除

--- pages/api/checkout.ts
+++ pages/api/checkout.ts
@@ -1,18 +1,18 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import Stripe from 'stripe';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_API_KEY!, {
   apiVersion: '2020-08-27',
-});
+})
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { price } = req.body;
+  const { price } = req.body
   // 注文アイテムの個数と、通貨を指定し、PaymentIntent を作成
   const paymentIntent = await stripe.paymentIntents.create({
     amount: price,
     currency: 'jpy',
-  });
+  })
   res.send({
     clientSecret: paymentIntent.client_secret,
-  });
-};
+  })
+}
